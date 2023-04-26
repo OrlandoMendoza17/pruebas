4537dd3d068cfea2899e261c595de46e8c20b497
Orlando Mendoza <ommv.17@gmail.com>
2023-04-26 16:59:46 -0400

    trying icalendar

---
+++ index.mjs
@@ -0,0 +1,10 @@
+import { getCalendarLinks } from "./utils/ICalendar.mjs"
+
+const info = {
+  informacion_fechaPublicacion: "2023-03-25",
+  informacion_fechaExpiracion: "2023-03-28",
+  informacion_titulo: "John Freddy Vega"
+}
+
+const links = getCalendarLinks(info)
+console.log("links:", links)
