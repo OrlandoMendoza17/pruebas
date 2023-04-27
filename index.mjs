d56d4e42e5df61d86e337974726c244c98afb713
Orlando Mendoza <ommv.17@gmail.com>
2023-04-27 16:41:11 -0400

    cambio de nombre al repositorio

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { getCalendarLinks } from "./utils/ICalendar.mjs"
 
 const info = {
   informacion_fechaPublicacion: "2023-03-25",
-  informacion_fechaExpiracion: "2023-03-28",
+  informacion_fechaExpiracion: "2023-11-02",
   informacion_titulo: "John Freddy Vega"
 }
 
