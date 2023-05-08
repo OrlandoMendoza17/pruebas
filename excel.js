ca0f9ec657fc78f5f38b46f8887a43090d493908
Orlando Mendoza <ommv.17@gmail.com>
2023-05-08 19:24:12 -0400

    Creación de archivo excel

--- excel.js
+++ excel.js
@@ -1,56 +1,70 @@
 const xl = require("excel4node")
 
-const workbook = new xl.Workbook()
-
-const worksheet = workbook.addWorksheet("Hoja 1")
-
-worksheet.cell(1, 1).string("Nombre")
-worksheet.cell(1, 2).string("Apellido")
-worksheet.cell(1, 3).string("Edad")
-worksheet.cell(1, 4).string("Profesión")
-
-const item = {
-  Compania: "07200",
-  No_batch: 3834963,
+const object = {
+  COMPANIA: "07200",
+  NO_BATCH: 3834963,
   PERIODO: 4,
-  Tipo: "JE",
+  TIPO: "JE",
   FECHA: "2023-04-25",
-  Documento: 5172023,
-  Cuenta: "        7200.116500.1120014",
-  N_Dir: 0,
-  Descripcion: "NOM:DIST. NOMINA ND P-17",
-  Monto: -5000,
-  Ult_Act: 161942,
-  Moneda: "VES",
-  glexa: "NOM:DIST. NOMINA ND P-17",
-  DocRef1: "00000000",
-  Tipo_Batch: "G"
+  DOCUMENTO: 5172023,
+  CUENTA: "        7200.116500.1120014",
+  N_DIR: 0,
+  DESCRIPCION: "NOM:DIST. NOMINA ND P-17",
+  MONTO: -5000,
+  ULT_ACT: 161942,
+  MONEDA: "VES",
+  GLEXA: "NOM:DIST. NOMINA ND P-17",
+  DOCREF1: "00000000",
+  TIPO_BATCH: "G"
 }
 
-const labels = Object.keys(item)
+const list = new Array(10).fill(object)
 
-labels.map((item, index) => {
-  const ROW = 1
-  const COLUMN = index + 1
-
-  worksheet.cell(ROW, COLUMN).string(item)
-})
-
-const array = new Array(10).fill(item)
-
-console.log(array);
-
-array.forEach((item, index) => {
-  const ROW = index + 2
-  const values = Object.values(item)
+const createExcelWorkbook = (list) =>{
+  const getTitles = (object) =>{
+    let titles = {}
+    const labelsList = Object.keys(object)
+    
+    // Se obtienen los nombres de cada campo se pasan a un objeto
+    labelsList.forEach(item =>{
+      titles = {
+        ...titles,
+        [item]: item,
+      }  
+    })
+    
+    return titles;
+  }
   
-  values.forEach((value, index) => {
-    
-    const valueType = typeof value;
-    
-    const COLUMN = index + 1
-    worksheet.cell(ROW, COLUMN)[valueType](value)
+  const titles = getTitles(list[0]) 
+  
+  //Se añaden los titulos de cada columna al array que se va imprimir en el excel
+  list.unshift(titles)
+  
+  console.log("list:", list);
+  
+  const workbook = new xl.Workbook()
+  const worksheet = workbook.addWorksheet("Hoja 1")
+  
+  list.forEach((item, index) => {
+    const ROW = index + 1;
+    const values = Object.values(item)
+  
+    // Se imprime cada fila de la tabla
+    values.forEach((value, index) => {
+      const COLUMN = index + 1
+      
+      // Se obtiene el tipo de valor para usar la función adecuada de la librería de excel en cada campo
+      const valueType = typeof value;
+  
+      // Se imprime la celda
+      worksheet.cell(ROW, COLUMN)[valueType](value)
+      
+    })
   })
-})
+  
+  return workbook;
+}
 
+const workbook = createExcelWorkbook(list)
 workbook.write("registros.xlsx")
