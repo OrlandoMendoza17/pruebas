325c1403bf8960fd44ecdc100f3d55b00fba998e
Orlando Mendoza <ommv.17@gmail.com>
2023-05-08 09:25:18 -0400

    Se pudo hacer conexión a la instancia de la base de datos

--- mssql.js
+++ mssql.js
@@ -2,34 +2,31 @@ require("dotenv").config()
 
 const sql = require('mssql')
 
-// const sqlConfig = {
-//   server: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   port: 1433,
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000
-//   },
-//   options: {
-//     encrypt: true, // for azure
-//     trustServerCertificate: true // change to true for local dev / self-signed certs
-//   }
-// }
-
-// console.log(sqlConfig);
-
 const DB_HOST = process.env.DB_HOST;
+const DB_INSTANCE = process.env.DB_INSTANCE;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-console.log("DB_HOST:", DB_HOST);
-console.log("DB_NAME:", DB_NAME);
-console.log("DB_USER:", DB_USER);
-console.log("DB_PASSWORD:", DB_PASSWORD);
+const sqlConfig = {
+  server: DB_HOST,
+  database: DB_NAME,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  // port: 1433,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    instanceName: DB_INSTANCE,
+    trustServerCertificate: true, // change to true for local dev / self-signed certs,
+    encrypt: false,
+  }
+}
+
+console.log(sqlConfig);
 console.log("");
 
 (async () => {
@@ -37,16 +34,13 @@ console.log("");
     
     // make sure that any items are correctly URL encoded in the connection string
     
-    // await sql.connect(sqlConfig)
-    await sql.connect(`Server=${DB_HOST},1433;Database=${DB_NAME};User Id=${DB_USER};Password=${DB_PASSWORD};Encrypt=true;`)
+    await sql.connect(sqlConfig)
+    // await sql.connect(`Server=${DB_HOST},1433;Database=${DB_NAME};User Id=${DB_USER};Password=${DB_PASSWORD};Encrypt=true;`)
     
     console.log("Conectado a la base de datos ✅");
     
-    // const result = await sql.query`SELECT * FROM dbo.tbl_persona WHERE persona_cedula = 27313279`
-    // console.log(result);
-    
-    // const result = await sql.query`select * from mytable where id = ${value}`
-    // console.dir(result)
+    const result = await sql.query`SELECT * FROM dbo.Paises`
+    console.log(result);
     
   } catch (error) {
     console.log("Error al conectarse a la base de datos ❌");
